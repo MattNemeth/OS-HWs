c3715627840ec7a6b833ad018bdb29c416df26ca
MattNemeth <nemeth.matthew@outlook.com>
2020-02-17 17:20:56 -0500

    completed problem one

--- HW2/problem_one.c
+++ HW2/problem_one.c
@@ -1,5 +1,15 @@
 //Problem One
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+
+//first arg: command, example "/bin/ps"
+//second arg: file, example "outputFile.txt"
+//	stores the output of the command passed in as arg 1
+//
 int main(int argc, char* argv[]){
 	if(argc > 3){
 		printf("Too many arguments... Program only takes 2 input arguments.\n");
@@ -11,9 +21,18 @@ int main(int argc, char* argv[]){
 	//	printf("The argument passed in must be an integer...\n");
 	//	exit(0);
 	}else{
-	//	printFunction(atoi(argv[1]));
-		fork();
-		exec(argv[1]);	//Execute the program that was passed in
+		int pid = fork();
+		if(pid == 0){ // if pid = 0 we know its a child process
+			int fd = open(argv[2], O_WRONLY | O_CREAT, S_IRUSR | S_IWUSR); 
+			if(fd < 0){
+				printf("There was an error opening the file.\n");
+			}
+
+			dup2(fd, 1);
+			close(fd);
+
+			execl(argv[1], argv[1], NULL); // execute the command that was passed in	
+		}
 	}
 	return 0;
 }
