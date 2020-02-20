5e036e570e06c6405623631f1cdb300e500a0da7
MattNemeth <nemeth.matthew@outlook.com>
2020-02-20 18:50:24 -0500

    can read all c files in current dir and sub dirs. need to add toupper for each .c file. added file for problem 3.

--- HW2/problem_two.c
+++ HW2/problem_two.c
@@ -6,13 +6,17 @@
 #include <fcntl.h>
 #include <sys/stat.h>
 
-int main(int argc, char* argv[]){
-	FILE * fp = popen("ls","r");
-	if(fp == 0){
-		printf("There was an error with the popen command...\n");
-	}else{
-			
-	}
+int main(){
+	FILE * fp1 = popen("find . -name '*.c'","r");
+	FILE * fp2 = popen("tr a-z A-Z","r");
 
+	char buf[20];
+	while(fgets(buf, 20, fp1) != NULL){
+		if(fp1 == 0){
+			printf("There was an error with the popen command...\n");
+		}else{
+			printf("%s", buf);	
+		}
+	}
 	return 0;
 }
